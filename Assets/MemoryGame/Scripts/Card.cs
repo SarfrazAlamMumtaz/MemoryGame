10c94dac17c1acd8efe6260b345e84b0d294b7a6
Sarfraz <45730667+SarfrazAlamMumtaz@users.noreply.github.com>
2025-10-02 22:55:17 +0530

    Remove unused using directives

--- Assets/MemoryGame/Scripts/Card.cs
+++ Assets/MemoryGame/Scripts/Card.cs
@@ -1,9 +1,7 @@
-﻿using Codice.Client.BaseCommands;
-using DG.Tweening;
+﻿using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
